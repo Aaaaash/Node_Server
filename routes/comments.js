1e93bd44e57346d37e1c59164c572f589516fbbb
SakuB <841105127@qq.com>
2016-12-13 18:22:01 +0800

    评论

--- routes/comments.js
+++ routes/comments.js
@@ -3,14 +3,22 @@ var router = express.Router();
 var CommentsModel = require('../lib/mongo').CommentsModel;
 
 // 获取单篇文章下评论
+router.get('/:articleID', function(req, res, next) {
+  var articleID = req.params.articleID;
+  CommentsModel.find({ articleID }, { __v: 0 }, function(err, data) {
+    if(err) throw err;
+    return res.status(200).json({ code: 0, data: data, message: 'ok', total: data.length });
+  });
+})
 
-// 提交评论
+;// 提交评论
 router.post('/', function(req, res, next) {
   var comment = {
     nickname: req.body.nickname,
     personalWebsite: req.body.personalWebsite,
     commentContent: req.body.commentContent,
     articleID: req.body.articleID,
+    createAt: parseInt(new Date().getTime()/1000),
   };
   CommentsModel.create(comment, function(err, info) {
     if(err) throw err;
