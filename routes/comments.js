a2bdd5b19b2e90f65d16b86f13a2f06146edf15f
SakuB <841105127@qq.com>
2016-12-14 13:10:45 +0800

    fix

--- routes/comments.js
+++ routes/comments.js
@@ -9,9 +9,9 @@ router.get('/:articleID', function(req, res, next) {
     if(err) throw err;
     return res.status(200).json({ code: 0, data: data, message: 'ok', total: data.length });
   });
-})
+});
 
-;// 提交评论
+// 提交评论
 router.post('/', function(req, res, next) {
   var comment = {
     nickname: req.body.nickname,
@@ -23,7 +23,7 @@ router.post('/', function(req, res, next) {
   CommentsModel.create(comment, function(err, info) {
     if(err) throw err;
     return res.status(200).json({ code: 0, message: 'ok' });
-  })
+  });
 });
 
 module.exports = router;
